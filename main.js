1a53496dab791b85b4858677b79672046d9f7518
Gabriel Spioni <gabrielprivate123@gmail.com>
2025-03-06 20:41:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
             <ul>
                 <li><a href="https://github.com/gatorabs" target="_blank">GitHub</a></li>
                 <li><a href="https://www.linkedin.com/in/gabriel-jos%C3%A9-spioni-estev%C3%A3o-742403235/" target="_blank">LinkedIn</a></li>
-                <li><a href="" target="_blank">Resume</a></li>
+                <li><a href="https://drive.google.com/file/d/1bkzUlNjgfZLJ6Ybokw7oN8X4zulfdbdH/view?usp=sharing" target="_blank">Resume</a></li>
                 <li><a href="mailto:gabrielprivate123@gmail.com" target="_blank">Email</a></li>
             </ul>
         `;
